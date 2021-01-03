58442fdf1f24be1a024e996e7bd2d015ef984873
Mark Ring <mjrcodin@gmail.com>
2021-01-02 17:09:01 -0700

    type json

--- apiv1/index.js
+++ apiv1/index.js
@@ -1,7 +1,9 @@
 let router = require('express').Router()
 let words = require('./w')
 router.get('/home',(req,res,next)=>res.send('<div id="page">Welcome to Index Page</div>'))
-router.get('/words',(req,res,next)=>res.json({words:words}))
+router.get('/words',(req,res,next)=>{
+    res.setHeader('Content-Type', 'application/json');
+    res.json({words:words})})
 router.get('/page2',(req,res,next)=>res.send('<h1 id="page">Hello From API 2</h1>'))
 router.get('/page3',(req,res,next)=>res.send('<h1 id="page">Hello From API 3</h1>'))
 
